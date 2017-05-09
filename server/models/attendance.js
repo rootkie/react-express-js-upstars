d0d478eab72bd95ce00f94c37ac8f82ff6d6b692
rootkid <rkxuanfeng@gmail.com>
2017-05-09 13:16:01 +0800

    created attendance model

---
+++ server/models/attendance.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+// ================================
+// User Schema
+// ================================
+const AttendanceSchema = new Schema({
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  hours: Number,
+  class: { type: Schema.ObjectId, ref: 'Class' },
+  tutors: { type: Schema.ObjectId, ref: 'User'},
+  students: { type: Schema.ObjectId, ref 'Student'},
+
+},
+  {
+    timestamps: true
+  })
+
+module.exports = mongoose.model('Attendance', AttendanceSchema)
