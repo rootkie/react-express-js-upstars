fa1a9849c2f03287edfa388181cdaac19ac03ec1
rootkid <rkxuanfeng@gmail.com>
2017-05-17 21:53:24 +0800

    Separated logic from get user/student attendance rate functions to individual functions so that it can be reused

--- server/controllers/StatisticsController.js
+++ server/controllers/StatisticsController.js
@@ -22,12 +22,8 @@ module.exports.getCipUser = async (req, res) => {
   }
 }
 
-module.exports.getAttendanceRateUser = async (req, res) => {
+async function genUserARStat (dateFrom, dateEnd, userId) {
   try {
-    let {dateFrom, dateEnd, userId} = req.body
-    dateFrom = util.makeString(dateFrom)
-    dateEnd = util.makeString(dateEnd)
-    userId = util.makeString(userId)
     let user = await User.findById(userId)
 
     let numOfClasses = user.classes.length
@@ -38,12 +34,25 @@ module.exports.getAttendanceRateUser = async (req, res) => {
       let classAttendance = await Attendance.find({ date: { '$gte': util.formatDate(dateFrom), '$lte': util.formatDate(dateEnd) }, class: user.classes[i] })
       let attendanceCount = classAttendance.length
       for (let j = 0; j < attendanceCount; j++) {
-        if (classAttendance[j].tutors.indexOf(userId) != -1) userAttendance += 1
+        if (classAttendance[j].tutors.indexOf(userId) !== -1) userAttendance += 1
       }
 
       totalAttendance += attendanceCount
     }
     const attendanceRate = (userAttendance / totalAttendance * 100).toFixed(2)
+    return attendanceRate
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+module.exports.getAttendanceRateUser = async (req, res) => {
+  try {
+    let {dateFrom, dateEnd, userId} = req.body
+    dateFrom = util.makeString(dateFrom)
+    dateEnd = util.makeString(dateEnd)
+    userId = util.makeString(userId)
+    const attendanceRate = await genUserARStat(dateFrom, dateEnd, userId)
     res.json({
       status: 'success',
       attendanceRate: attendanceRate
@@ -54,29 +63,36 @@ module.exports.getAttendanceRateUser = async (req, res) => {
   }
 }
 
+async function genStudentARStat (dateFrom, dateEnd, studentId) {
+  try {
+    let student = await Student.findById(studentId)
+    let numOfClasses = student.classes.length
+
+    let totalAttendance = 0
+    let studentAttendance = 0
+    
+    for (let i = 0; i < numOfClasses; i++) {
+      let classAttendance = await Attendance.find({ date: { '$gte': util.formatDate(dateFrom), '$lte': util.formatDate(dateEnd) }, class: student.classes[i] })
+      let attendanceCount = classAttendance.length
+      for (let j = 0; j < attendanceCount; j++) {
+        if (classAttendance[j].students.indexOf(studentId) !== -1) studentAttendance += 1
+      }
+      totalAttendance += attendanceCount
+    }
+    const attendanceRate = (studentAttendance / totalAttendance * 100).toFixed(2)
+    return attendanceRate
+  } catch (err) {
+    throw err
+  }
+}
+
 module.exports.getAttendanceRateStudent = async (req, res) => {
   try {
     let {dateFrom, dateEnd, studentId} = req.body
     dateFrom = util.makeString(dateFrom)
     dateEnd = util.makeString(dateEnd)
     studentId = util.makeString(studentId)
-    let student = await Student.findById(studentId)
-    let numOfClasses = student.classes.length
-
-    let totalAttendance = 0
-    let studentAttendance = 0
-    console.log(studentId)
-    for (let i = 0; i < numOfClasses; i++) {
-      let classAttendance = await Attendance.find({ date: { '$gte': util.formatDate(dateFrom), '$lte': util.formatDate(dateEnd) }, class: student.classes[i] })
-      let attendanceCount = classAttendance.length
-      for (let j = 0; j < attendanceCount; j++) {
-        if (classAttendance[j].students.indexOf(studentId) != -1) studentAttendance += 1
-      }
-      totalAttendance += attendanceCount
-    }
-    console.log(totalAttendance)
-    console.log(studentAttendance)
-    const attendanceRate = (studentAttendance / totalAttendance * 100).toFixed(2)
+    const attendanceRate = await genStudentARStat(dateFrom, dateEnd, studentId)
     res.json({
       status: 'success',
       attendanceRate: attendanceRate
