0b62df4855b14e6a788459154f5167a4bfaa5067
wonyk <yingkeatwon@gmail.com>
2017-05-23 09:34:59 +0000

    Added change password function

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -31,12 +31,78 @@ module.exports.editUserParticulars = async(req, res) => {
     }
 }
 
-module.exports.changePassword = function(req, res) {
-
+module.exports.changePassword = async(req, res) => {
+    try {
+        let {
+            userId,
+            oldPassword,
+            newPassword,
+            confirmNewPassword
+        } = req.body
+        userId = util.makeString(userId)
+        oldPassword = util.makeString(oldPassword)
+        newPassword = util.makeString(newPassword)
+        confirmNewPassword = util.makeString(confirmNewPassword)
+            //Just in case the front end screws up
+        if (newPassword != confirmNewPassword) {
+            res.status(422).send({
+                error: 'The 2 new passwords do not match'
+            })
+        }
+        else {
+            const user = await User.findById(userId)
+            const isMatch = await user.comparePasswordPromise(oldPassword)
+            if (!isMatch) {
+                res.status(403).send('Wrong Password')
+            }
+            // Did not return the token. Pls add in so its standardised. I dont want to add the wrong things
+            else {
+                user.password = confirmNewPassword
+                const pwChanged = await user.save();
+                return res.json({
+                    user: pwChanged
+                        // token: ...
+                })
+            }
+        }
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send('server error')
+    }
 }
 
-module.exports.adminChangePassword = function(req, res) {
-
+module.exports.adminChangePassword = async(req, res) => {
+    try {
+        let {
+            userId,
+            newPassword,
+            confirmNewPassword
+        } = req.body
+        userId = util.makeString(userId)
+        newPassword = util.makeString(newPassword)
+        confirmNewPassword = util.makeString(confirmNewPassword)
+            //Just in case the front end screws up
+        if (newPassword != confirmNewPassword) {
+            res.status(422).send({
+                error: 'The 2 new passwords do not match'
+            })
+        }
+        else {
+            const user = await User.findById(userId)
+                // Did not return the token. Pls add in so its standardised. I dont want to add the wrong things
+            user.password = confirmNewPassword
+            const pwChanged = await user.save();
+            return res.json({
+                user: pwChanged
+                    // token:
+            })
+        }
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send('server error')
+    }
 }
 
 module.exports.changeUserStatusAndPermissions = function(req, res) {
