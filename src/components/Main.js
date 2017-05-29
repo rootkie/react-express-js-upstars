cb9569c9797398fafcdad202206072235ce36ed8
rayduck <ruida@dingansi.ch>
2017-05-29 21:50:14 +0800

    updates UI

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-import { Container, Grid, Segment, Menu } from 'semantic-ui-react'
+import { Container, Grid, Segment, Menu, Dropdown } from 'semantic-ui-react'
 
 const GridStyle = {
   paddingTop: '4em',
   height: '100vh',
-  overflow: 'none'
+  overflow: 'none',
+  margin: '0'
 }
 
 const MainContentStyle = {
@@ -21,18 +22,37 @@ class Main extends Component {
     return (
       <Container fluid>
         <Menu size='large' fixed='top'>
-          <Menu.Item header>UPStars</Menu.Item>
+          <Menu.Item header width={2}>UPStars</Menu.Item>
+          <Menu.Item header>{activeItem}</Menu.Item>
           <Menu.Menu position='right'>
-            <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
-            <Menu.Item name='class' active={activeItem === 'class'} onClick={this.handleItemClick} />
+            <Dropdown item text='Username'>
+              <Dropdown.Menu>
+                <Dropdown.Item>Settings</Dropdown.Item>
+                <Dropdown.Item>Logout</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
           </Menu.Menu>
         </Menu>
         <Grid style={GridStyle}>
           <Grid.Column width={3}>
-            <Menu pointing secondary vertical fluid>
+            <Menu vertical fluid>
               <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
               <Menu.Item name='messages' active={activeItem === 'messages'} onClick={this.handleItemClick} />
-              <Menu.Item name='class' active={activeItem === 'class'} onClick={this.handleItemClick} />
+              <Menu.Item>
+                <Menu.Header>Class</Menu.Header>
+
+                <Menu.Menu>
+                  <Menu.Item name='addClass' active={activeItem === 'addClass'} onClick={this.handleItemClick}>
+                    add
+                  </Menu.Item>
+                  <Menu.Item name='editClass' active={activeItem === 'editClass'} onClick={this.handleItemClick}>
+                    edit
+                  </Menu.Item>
+                  <Menu.Item name='viewClass' active={activeItem === 'viewClass'} onClick={this.handleItemClick}>
+                    view
+                  </Menu.Item>
+                </Menu.Menu>
+              </Menu.Item>
             </Menu>
           </Grid.Column>
           <Grid.Column width={13} style={MainContentStyle}>
