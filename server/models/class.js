9ac6558e45bce5356222d106064aa6716a5287ce
rootkid <rkxuanfeng@gmail.com>
2017-06-11 11:31:49 +0800

    Updates class model

--- server/models/class.js
+++ server/models/class.js
@@ -4,14 +4,29 @@ const Schema = mongoose.Schema
 // Class Student Schema
 // ================================
 const ClassSchema = new Schema({
+  // Class information
   className: {
     type: String,
-    unique: true,
     required: true
   },
-  description: {
-    type: String
+  classType: {
+    type: String,
+    enum: ['Tuition','Enrichment'],
+    require: true
   },
+  venue: {
+    type: String,
+    require: true
+  },
+  dayAndTime: {
+    type: String,
+    require: true
+  },
+  startDate: {
+    type: Date,
+    require: true
+  },
+  // Refs to other models
   students: [{
     type: Schema.ObjectId,
     ref: 'Student'
