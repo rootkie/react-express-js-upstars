7990526b4775345a0ae1803cd68a4caa050b9990
rootkid <rkxuanfeng@gmail.com>
2017-07-29 00:02:27 +0800

    Mostly completed registration form for both student and volunteer

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Main from './components/Main'
 import MainCtrl from './components/MainCtrl'
-import Register from './components/Register/index'
+import StudentRegister from './components/Register/StudentRegisterForm'
+import VolunteerRegister from './components/Register/VolunteerRegisterForm'
 import Login from './components/Login'
 import 'semantic-ui-css/semantic.min.css'
 import './index.css'
@@ -13,7 +14,8 @@ const Root = () => (
     <div id='page-layout'>
       <Route exact path='/' component={Main} />
       <Switch>
-        <Route path='/register' component={Register} />
+        <Route path='/register/student' component={StudentRegister} />
+        <Route path='/register/volunteer' component={VolunteerRegister} />
         <Route path='/login' component={Login} />
         <Route path='/:main/:op?' component={MainCtrl} />
       </Switch>
