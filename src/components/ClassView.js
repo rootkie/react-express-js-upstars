f0ffb46de4c32de0c88ea33620ea11e17ab20947
rootkid <rkxuanfeng@gmail.com>
2017-08-01 20:42:17 +0800

    get and display classes

--- src/components/ClassView.js
+++ src/components/ClassView.js
@@ -1,28 +1,60 @@
 import React, {Component} from 'react'
 import { Table, Checkbox, Button, Icon, Link, Dropdown, Confirm } from 'semantic-ui-react'
+import axios from 'axios'
 
-const classData = [
-  {_id: '1', className:'test', dayAndTime: 'monday 9pm', venue:'somewhere', classType: 'Tuition'},
-  {_id: '2', className:'test2', dayAndTime: 'monday 9pm', venue:'somewhere', classType: 'Tuition'},
-  {_id: '3', className:'test3', dayAndTime: 'monday 9pm', venue:'somewhere', classType: 'Tuition'},
-]
+axios.defaults.baseURL = 'https://test.rootkiddie.com/api/'
 
 class ClassView extends Component {
-  state = {
-    classData
+  
+  constructor(props) {
+    super()
+    // This is probably where to get all the necessary data from server api
+    this.state = {
+      selected: [],
+      classData: []
+    }
+    this.getClasses()
+  }
+
+  getClasses = () => {
+    axios.get('class')
+      .then((response) => {
+        this.setState({ classData: response.data.classes })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  handleCheckBox = (e, { name, checked }) => {
+    let { selected } = this.state
+    if (checked) {
+      selected.push(name) // name here is actually class _id
+    } else {
+      selected = selected.filter((element) => element !== name)
+    }
+    this.setState({selected})
+  }
+
+  handleEdit = () => {
+    console.log(this.state.selected)
+  }
+
+  handleDelete = () => {
+    console.log(this.state.selected)
   }
 
   render() {
-    const {classData} = this.state
+    const { classData, selected } = this.state
     return (
       <Table compact celled>
         <Table.Header>
           <Table.Row>
             <Table.HeaderCell />
             <Table.HeaderCell>Class Name</Table.HeaderCell>
+            <Table.HeaderCell>classType</Table.HeaderCell>
             <Table.HeaderCell>Day and Time</Table.HeaderCell>
             <Table.HeaderCell>venue</Table.HeaderCell>
-            <Table.HeaderCell>classType</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
 
@@ -30,12 +62,12 @@ class ClassView extends Component {
           {classData.map((Class, i) => (
             <Table.Row key={`class-${i}`}>
               <Table.Cell collapsing>
-                <Checkbox name={Class._id} />
+                <Checkbox name={Class._id} onChange={this.handleCheckBox} checked={selected.includes(Class._id)} />
               </Table.Cell>
               <Table.Cell>{Class.className}</Table.Cell>
+              <Table.Cell>{Class.classType}</Table.Cell>
               <Table.Cell>{Class.dayAndTime}</Table.Cell>
               <Table.Cell>{Class.venue}</Table.Cell>
-              <Table.Cell>{Class.classType}</Table.Cell>
             </Table.Row>))}
         </Table.Body>
         <Table.Footer fullWidth>
@@ -46,7 +78,7 @@ class ClassView extends Component {
                 <Icon name='group' />New Class
               </Button>
               <Button size='small' negative>Delete</Button>
-              <Button size='small'>Edit</Button>
+              <Button size='small' onClick={this.handleEdit}>Edit</Button>
               <Confirm />
             </Table.HeaderCell>
           </Table.Row>
