be9a24cd938a0c5ea36ef4e486a095382ce155fc
wonyk <yingkeatwon@gmail.com>
2018-03-29 22:29:10 +0800

    Done with attendance fully
    Working on:
    1. Student view bug
    2. User Profile management

--- src/components/Attendance/AttendanceClass.js
+++ src/components/Attendance/AttendanceClass.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Table, Form, Dropdown, Icon, Header, Pagination } from 'semantic-ui-react'
+import { Table, Form, Dropdown, Icon, Header, Pagination, Dimmer, Loader } from 'semantic-ui-react'
 import { array } from 'prop-types'
 import moment from 'moment'
 import axios from 'axios'
@@ -10,6 +10,8 @@ class AttendanceClass extends Component {
   }
   constructor (props) {
     super(props)
+    // Basic Stats contains the non-changing details as well as the filtered dates to be displayed
+    // student and tutor attendance contains filtered attendance data taking account of the pages and number to be displayed.
     this.state = {
       classSelector: '',
       basicStats: {
@@ -19,43 +21,88 @@ class AttendanceClass extends Component {
       },
       totalPages: 1,
       activePage: 1,
+      rawClassData: [],
       studentAttendance: [],
-      tutorAttendance: []
+      tutorAttendance: [],
+      isLoading: false
     }
   }
 
   handleSubmit = (e) => {
     e.preventDefault()
+    this.setState({isLoading: true})
     axios.get(`/attendance/${this.state.classSelector}/summary`)
       .then(response => {
-        console.log(response)
-        let pagesRequired = Math.ceil(response.data.attendanceDates.length / 8)
+        // Calculate pages required by rounding up the number divided by 6 (6 sets of data / page)
+        let pagesRequired = Math.ceil(response.data.attendanceDates.length / 6)
         let rawClassData = response.data
-        const indexOfLast = this.state.activePage * 8
-        const indexOfFirst = indexOfLast - 8
-        let attendanceDates = rawClassData.attendanceDates.slice(indexOfFirst, indexOfLast)
-        let basicStats = {
-          studentNumber: rawClassData.studentNumber,
-          tutorNumber: rawClassData.tutorNumber,
-          studentTutorRatio: rawClassData.studentTutorRatio,
-          status: rawClassData.status,
-          attendanceDates
-        }
-        let studentAttendance = rawClassData.compiledStudentAttendance.map((user, index) => {
-          return {
-            details: user.details.slice(indexOfFirst, indexOfLast),
-            attended: user.attended,
-            percentage: user.percentage,
-            studentName: user.studentName[0],
-            studentID: user.studentID,
-            total: user.total
-          }
+        let editedClassData = this.processData(rawClassData, 1)
+        this.setState({
+          basicStats: editedClassData.basicStats,
+          totalPages: pagesRequired,
+          studentAttendance: editedClassData.studentAttendance,
+          tutorAttendance: editedClassData.tutorAttendance,
+          rawClassData,
+          isLoading: false
         })
-        this.setState({basicStats, totalPages: pagesRequired, studentAttendance})
+      })
+      .catch(error => {
+        console.log(error)
       })
   }
 
-  handlePaginationChange = (e, { activePage }) => this.setState({ activePage })
+  processData = (rawData, activePage) => {
+    // They calculate the index so that we can slice off the necesary data based on the current page user selects
+    const indexOfLast = activePage * 6
+    const indexOfFirst = indexOfLast - 6
+    let attendanceDates = rawData.attendanceDates.slice(indexOfFirst, indexOfLast)
+    let basicStats = {
+      studentNumber: rawData.studentNumber,
+      tutorNumber: rawData.tutorNumber,
+      studentTutorRatio: rawData.studentTutorRatio,
+      status: rawData.status,
+      attendanceDates
+    }
+    let studentAttendance = rawData.compiledStudentAttendance.map((user, index) => {
+      return {
+        details: user.details.slice(indexOfFirst, indexOfLast),
+        attended: user.attended,
+        percentage: user.percentage,
+        studentName: user.studentName[0],
+        studentID: user.studentID,
+        total: user.total
+      }
+    })
+    let tutorAttendance = rawData.compiledUserAttendance.map((user, index) => {
+      return {
+        details: user.details.slice(indexOfFirst, indexOfLast),
+        attended: user.attended,
+        percentage: user.percentage,
+        userName: user.userName[0],
+        userID: user.userID,
+        total: user.total
+      }
+    })
+    return {
+      studentAttendance,
+      tutorAttendance,
+      basicStats
+    }
+  }
+
+  handlePaginationChange = (e, { activePage }) => {
+    e.preventDefault()
+    this.setState({isLoading: true})
+    // Here, the same processData function is called.
+    let editedClassData = this.processData(this.state.rawClassData, activePage)
+    this.setState({
+      basicStats: editedClassData.basicStats,
+      studentAttendance: editedClassData.studentAttendance,
+      tutorAttendance: editedClassData.tutorAttendance,
+      activePage,
+      isLoading: false
+    })
+  }
 
   // Real-time API call to search for the data.
   handleSearchOptions = (e, { name, value }) => {
@@ -63,11 +110,14 @@ class AttendanceClass extends Component {
   }
 
   render () {
-    const { classSelector, basicStats, totalPages, studentAttendance } = this.state
+    const { classSelector, basicStats, totalPages, studentAttendance, tutorAttendance, isLoading } = this.state
     const { classData } = this.props
 
     return (
       <div>
+        <Dimmer active={isLoading} inverted>
+          <Loader indeterminate active={isLoading}>Loading Data</Loader>
+        </Dimmer>
         <Table celled striped>
           <Table.Header>
             <Table.Row>
@@ -111,22 +161,40 @@ class AttendanceClass extends Component {
             Full Attendance Records
           </Header.Content>
         </Header>
-        <Table celled striped columns={12}>
+        {/* 11 columns (5 for basic data and 6 for attendance records per page) */}
+        {/* 6 sets is tested on computer resolutions of min 1280 x 720 to be able to see everything */}
+        <Table celled striped columns={11}>
           <Table.Header>
             <Table.Row>
-              <Table.HeaderCell>Tutor Name</Table.HeaderCell>
+              <Table.HeaderCell>Name</Table.HeaderCell>
+              <Table.HeaderCell>Role</Table.HeaderCell>
               <Table.HeaderCell>Total Class</Table.HeaderCell>
               <Table.HeaderCell>No. attendeed</Table.HeaderCell>
               <Table.HeaderCell>Percentage</Table.HeaderCell>
+              {/* Display attendance dates (6 sets per page) */}
               {basicStats.status === 'success' && basicStats.attendanceDates.map((date, index) => (
                 <Table.HeaderCell>{moment(date.date).format('L')}</Table.HeaderCell>
               ))}
             </Table.Row>
           </Table.Header>
           <Table.Body>
+            {/* nested maps allow the various details to be displayed out in order */}
             {basicStats.status === 'success' && studentAttendance.map((attendance, index) => (
-              <Table.Row key={`attendance-${index}`}>
+              <Table.Row key={`attendanceStudent-${index}`}>
                 <Table.Cell>{attendance.studentName}</Table.Cell>
+                <Table.Cell>Student</Table.Cell>
+                <Table.Cell>{attendance.total}</Table.Cell>
+                <Table.Cell>{attendance.attended}</Table.Cell>
+                <Table.Cell>{(attendance.percentage * 100).toFixed(2)}%</Table.Cell>
+                {attendance.details.map((individualStatus, index) => (
+                  <Table.Cell>{individualStatus}</Table.Cell>
+                ))}
+              </Table.Row>
+            ))}
+            {basicStats.status === 'success' && tutorAttendance.map((attendance, index) => (
+              <Table.Row key={`attendanceUser-${index}`} positive>
+                <Table.Cell>{attendance.userName}</Table.Cell>
+                <Table.Cell>Tutor</Table.Cell>
                 <Table.Cell>{attendance.total}</Table.Cell>
                 <Table.Cell>{attendance.attended}</Table.Cell>
                 <Table.Cell>{(attendance.percentage * 100).toFixed(2)}%</Table.Cell>
@@ -138,7 +206,7 @@ class AttendanceClass extends Component {
           </Table.Body>
           <Table.Footer>
             <Table.Row>
-              <Table.HeaderCell colSpan='12'>
+              <Table.HeaderCell colSpan='11'>
                 <Pagination defaultActivePage={1} totalPages={totalPages} floated='right' onPageChange={this.handlePaginationChange} />
               </Table.HeaderCell>
             </Table.Row>
