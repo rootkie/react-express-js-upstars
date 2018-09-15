5d87d55b4970a290abdb80f3302e8e3835aa8ddb
wonyk <yingkeatwon@gmail.com>
2018-09-15 15:07:34 +0800

    Prep for 0.3.0-beta production push

--- src/components/MainCtrl.js
+++ src/components/MainCtrl.js
@@ -15,8 +15,8 @@ import FourZeroThree from './Error/403'
 import FourZeroFour from './Error/404'
 import FiveHundred from './Error/500'
 
-// axios.defaults.baseURL = 'https://test.rootkiddie.com/api/'
-axios.defaults.baseURL = 'http://127.0.0.1:3000/api/'
+axios.defaults.baseURL = 'https://test.rootkiddie.com/api/'
+// axios.defaults.baseURL = 'http://127.0.0.1:3000/api/'
 axios.defaults.headers.common['x-access-token'] = window.localStorage.token
 axios.defaults.headers.post['Content-Type'] = 'application/json'
 
