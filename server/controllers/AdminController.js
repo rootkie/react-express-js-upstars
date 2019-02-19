48bbdc1a83ded78cccbf08155061d48e25076bcd
wonyk <yingkeatwon@gmail.com>
2019-02-19 11:13:16 +0800

    Refined Admin and Statistics #75 - [Changed] AdminController - [Changed] StatisticsController

--- server/controllers/AdminController.js
+++ server/controllers/AdminController.js
@@ -2,15 +2,10 @@ const User = require('../models/user')
 const Class = require('../models/class')
 
 module.exports.changeUserStatusAndPermissions = async (req, res, next) => {
+  const { userId, newStatus, newRoles } = req.body
   try {
-    let {
-      userId,
-      newStatus,
-      newRoles
-    } = req.body
     let edited = {}
     let editedClass
-    // Check if these fields exist, if it does it will get updated in the database
     if (newStatus) {
       edited.status = newStatus
     }
@@ -23,39 +18,41 @@ module.exports.changeUserStatusAndPermissions = async (req, res, next) => {
       runValidators: true
     })
     if (!updatedUser) {
-      throw ({
+      const error = {
         status: 404,
         error: 'User does not exist'
-      })
+      }
+      throw error
     }
-    // Add the user back to the previous classes if admin restores Suspended or Deleted Account.
-    // Else, the user would be deleted from the respective classes if they are suspended or deleted.
-    if (updatedUser.status === 'Active' && updatedUser.classes) {
-      editedClass = await Class.update({
-        _id: {
-          $in: updatedUser.classes
-        }
-      }, {
-        $addToSet: {
-          users: updatedUser._id
-        }
-      }, {
-        new: true,
-        multi: true
-      })
-    } else if (updatedUser.classes) {
-      editedClass = await Class.update({
-        _id: {
-          $in: updatedUser.classes
-        }
-      }, {
-        $pull: {
-          users: updatedUser._id
-        }
-      }, {
-        new: true,
-        multi: true
-      })
+    // Only if there is a change in status we then call the API to update the classes
+    if (newStatus) {
+      if (updatedUser.status === 'Active' && updatedUser.classes) {
+        editedClass = await Class.update({
+          _id: {
+            $in: updatedUser.classes
+          }
+        }, {
+          $addToSet: {
+            users: updatedUser._id
+          }
+        }, {
+          new: true,
+          multi: true
+        })
+      } else if (updatedUser.status !== 'Active' && updatedUser.classes) {
+        editedClass = await Class.update({
+          _id: {
+            $in: updatedUser.classes
+          }
+        }, {
+          $pull: {
+            users: updatedUser._id
+          }
+        }, {
+          new: true,
+          multi: true
+        })
+      }
     }
 
     // Returns necessary information
@@ -63,7 +60,7 @@ module.exports.changeUserStatusAndPermissions = async (req, res, next) => {
       editedClass
     })
   } catch (err) {
-    console.log(err)
+    console.error(err)
     if (err.name === 'ValidationError') {
       res.status(400).send({
         error: 'There is something wrong with the client input. That is all we know.'
@@ -81,12 +78,12 @@ module.exports.getPendingUsers = async (req, res, next) => {
     // Find all users with status as Pending
     const users = await User.find({
       'status': 'Pending'
-    }).limit(100).select('profile.name roles status').sort('profile.name').lean()
+    }).limit(100).select('name roles status').sort('name').lean()
     res.status(200).json({
       users
     })
   } catch (err) {
-    console.log(err)
+    console.error(err)
     next(err)
   }
 }
@@ -96,46 +93,43 @@ module.exports.getSuspendedPeople = async (req, res, next) => {
     // Find all people with status as Suspended
     const users = await User.find({
       'status': 'Suspended'
-    }).limit(100).select('profile.name roles status').sort('profile.name').lean()
+    }).limit(100).select('name roles status').sort('name').lean()
     res.status(200).json({
       users
     })
   } catch (err) {
-    console.log(err)
+    console.error(err)
     next(err)
   }
 }
 
 module.exports.getDeletedPeople = async (req, res, next) => {
   try {
-    // Find all people with status as Suspended
+    // Find all people with status as Deleted
     const users = await User.find({
       'status': 'Deleted'
-    }).limit(100).select('profile.name roles status').sort('profile.name').lean()
+    }).limit(100).select('name roles status').sort('name').lean()
     res.status(200).json({
       users
     })
   } catch (err) {
-    console.log(err)
+    console.error(err)
     next(err)
   }
 }
 
 module.exports.multipleUserDelete = async (req, res, next) => {
-  let {
-    userId
-  } = req.body
+  const { userId } = req.body
   try {
     // Check userId is provided
-    if (!userId || userId.length === 0 || !(/^[0-9a-fA-F]{24}$/).test(userId)) {
-      throw ({
+    if (!userId || userId.length === 0 || !userId.every(id => (/^[0-9a-fA-F]{24}$/).test(id))) {
+      const error = {
         status: 400,
-        error: 'Please provide a userId and ensure input is correct'
-      })
+        error: 'Please provide working userId(s)'
+      }
+      throw error
     }
 
-    // Delete user from database
-    // Find a user whose status is not previously deleted to change it to delete (Note: $ne == not equals)
     const userDeleted = await User.update({
       '_id': {
         '$in': userId
@@ -148,36 +142,34 @@ module.exports.multipleUserDelete = async (req, res, next) => {
     }, {
       multi: true
     })
-    // If theres actually someone deleted, loop through and delete the userId from the respective classes so that the population of the class would
-    // go well. Once the status of the user is changed back to Active, the classes of the user would be automatically restored. Even if the user
-    // is really deleted and a new account is created, the system would not crash.
+    // If theres actually someone deleted, loop through and delete the userId from the classes each user is in
     if (userDeleted.n === 0) {
-      throw ({
+      const error = {
         status: 404,
         error: 'The user you requested to delete does not exist.'
-      })
-    } else {
-      for (let number = 0; number < userId.length; number++) {
-        let userDetails = await User.findById(userId[number], 'classes')
-        if (userDetails.classes) {
-          await Class.update({
-            _id: {
-              $in: userDetails.classes
-            }
-          }, {
-            $pull: {
-              users: userDetails._id
-            }
-          }, {
-            new: true,
-            multi: true
-          })
-        }
+      }
+      throw error
+    }
+    for (let number = 0; number < userId.length; number++) {
+      const userDetails = await User.findById(userId[number], 'classes')
+      if (userDetails.classes) {
+        await Class.update({
+          _id: {
+            $in: userDetails.classes
+          }
+        }, {
+          $pull: {
+            users: userDetails._id
+          }
+        }, {
+          new: true,
+          multi: true
+        })
       }
     }
-    return res.status(200).send()
+    res.status(200).send()
   } catch (err) {
-    console.log(err)
+    console.error(err)
     if (err.status) {
       res.status(err.status).send({
         error: err.error
@@ -187,28 +179,28 @@ module.exports.multipleUserDelete = async (req, res, next) => {
 }
 
 module.exports.responsiveSearch = async (req, res, next) => {
-  let { name } = req.params
+  const { name } = req.params
   try {
     // Find all users with that name of whatever status
     const pendingMatched = await User.find({
       'status': 'Pending',
-      'profile.name': new RegExp(name, 'i')
-    }).limit(50).select('profile.name roles status').sort('profile.name').lean()
+      'name': new RegExp(name, 'i')
+    }).limit(50).select('name roles status').sort('name').lean()
 
     const activeMatched = await User.find({
       'status': 'Active',
-      'profile.name': new RegExp(name, 'i')
-    }).limit(50).select('profile.name roles status').sort('profile.name').lean()
+      'name': new RegExp(name, 'i')
+    }).limit(50).select('name roles status').sort('name').lean()
 
     const suspendedMatched = await User.find({
       'status': 'Suspended',
-      'profile.name': new RegExp(name, 'i')
-    }).limit(50).select('profile.name roles status').sort('profile.name').lean()
+      'name': new RegExp(name, 'i')
+    }).limit(50).select('name roles status').sort('name').lean()
 
     const deletedMatched = await User.find({
       'status': 'Deleted',
-      'profile.name': new RegExp(name, 'i')
-    }).limit(50).select('profile.name roles status').sort('profile.name').lean()
+      'name': new RegExp(name, 'i')
+    }).limit(50).select('name roles status').sort('name').lean()
 
     res.status(200).json({
       pendingMatched,
@@ -217,7 +209,7 @@ module.exports.responsiveSearch = async (req, res, next) => {
       deletedMatched
     })
   } catch (err) {
-    console.log(err)
+    console.error(err)
     next(err)
   }
 }
