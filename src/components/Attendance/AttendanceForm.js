d85111fc21c6c4999378f3a9be5b79ee8c702171
wonyk <yingkeatwon@gmail.com>
2019-04-28 14:26:51 +0800

    - [Changed] AttendanceView to Hooks - [Added] Shared AddEdit form for both functions

--- src/components/Attendance/AttendanceForm.js
+++ src/components/Attendance/AttendanceForm.js
@@ -1,10 +1,9 @@
 import React, { useReducer } from 'react'
-import { Form, Message, Header, Table, Checkbox, Loader, Dimmer, Grid } from 'semantic-ui-react'
+import { Form, Message, Loader, Dimmer, Grid } from 'semantic-ui-react'
 import PropTypes from 'prop-types'
-import DatePicker from 'react-datepicker'
+import AddEditForm from './shared/AddEditForm'
 import { object, string, date, number } from 'yup'
 import axios from 'axios'
-import moment from 'moment'
 
 // Note: classSelection is a bool that tracks if any class is provided by the user.
 const initialState = {
@@ -12,18 +11,12 @@ const initialState = {
   className: '',
   type: 'Class',
   hours: '',
-  students: [{'text': 'Select class to view sudents', 'key': '0'}],
-  users: [{'text': 'Select class to view users', 'key': '1'}],
+  students: [{text: 'Select class to view sudents', key: '0'}],
+  users: [{text: 'Select class to view users', key: '0'}],
   isLoading: false,
   classSelection: false,
   error: ''
 }
-
-const typeOptions = [
-  { key: 'Class', text: 'Class', value: 'Class' },
-  { key: 'PHoliday', text: 'Public Holiday', value: 'PHoliday' },
-  { key: 'Cancelled', text: 'Cancelled', value: 'Cancelled' }
-]
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -66,24 +59,6 @@ const reducer = (state, action) => {
 const AttendanceForm = ({classData, history}) => {
   const [state, dispatch] = useReducer(reducer, initialState)
 
-  const handleCheckboxChangeForUser = (e, { name: id, checked }) => {
-    let users = [...state.users]
-    const pos = users.map(usr => usr.key).indexOf(id)
-    users[pos].status = checked ? 1 : 0
-    dispatch({type: 'updateField', name: 'users', value: users})
-  }
-
-  const handleCheckboxChangeForStudent = (e, { name: id, checked }) => {
-    let students = [...state.students]
-    const pos = students.map(std => std.key).indexOf(id)
-    students[pos].status = checked ? 1 : 0
-    dispatch({type: 'updateField', name: 'students', value: students})
-  }
-
-  const handleChange = (e, { name, value }) => {
-    dispatch({type: 'updateField', name, value})
-  }
-
   const handleClass = (e, { value }) => {
     dispatch({type: 'retrieveData', value})
     axios.get('/class/' + value)
@@ -118,29 +93,6 @@ const AttendanceForm = ({classData, history}) => {
       })
   }
 
-  const handleChangeType = (e, { value }) => {
-    dispatch({type: 'updateField', name: 'type', value})
-    let students = [...state.students]
-    let users = [...state.users]
-    if (value === 'Class') {
-      for (let a = 0; a < students.length; a++) {
-        students[a]['status'] = 1
-      }
-      for (let b = 0; b < users.length; b++) {
-        users[b]['status'] = 1
-      }
-    } else {
-      for (let a = 0; a < students.length; a++) {
-        students[a]['status'] = 0
-      }
-      for (let b = 0; b < users.length; b++) {
-        users[b]['status'] = 0
-      }
-      dispatch({type: 'updateField', name: 'hours', value: 0})
-    }
-    dispatch({type: 'changeType', users, students})
-  }
-
   const handleSubmit = e => {
     e.preventDefault()
     const { attendanceDate, className, type, students, users, hours } = state
@@ -169,13 +121,12 @@ const AttendanceForm = ({classData, history}) => {
       }
     }).catch(err => {
       if (err.name === 'ValidationError') {
-        console.log(err.errors)
         dispatch({type: 'updateField', name: 'error', value: err.errors})
       }
     })
   }
 
-  const { attendanceDate, type, className, students, users, error, hours, classSelection, isLoading } = state
+  const { error, classSelection, isLoading } = state
   if (isLoading) {
     return (
       <Dimmer active={isLoading} inverted>
@@ -188,77 +139,7 @@ const AttendanceForm = ({classData, history}) => {
       <Grid.Row>
         <Grid.Column>
           <Form onSubmit={handleSubmit}>
-            <Form.Group widths='equal'>
-              <Form.Select label='Class' placeholder='Name of class' name='className' options={classData} search selection minCharacters={0} value={className} onChange={handleClass} required />
-              <Form.Select label='Type' placeholder='Type' name='type' options={typeOptions} value={type} onChange={handleChangeType} disabled={!classSelection} required />
-              <Form.Input label='Hours' placeholder='enter hours here' type='number' name='hours' value={hours} onChange={handleChange} disabled={type !== 'Class' || !classSelection} required={type === 'Class'} />
-            </Form.Group>
-            <Form.Group widths='equal'>
-              <Form.Field required>
-                <label>Date of class</label>
-                <DatePicker
-                  placeholderText='Click to select a date'
-                  dateFormat='DD/MM/YYYY'
-                  disabled={!classSelection}
-                  selected={attendanceDate}
-                  maxDate={moment()}
-                  onChange={date => { dispatch({type: 'updateField', name: 'attendanceDate', value: date}) }}
-                  required />
-              </Form.Field>
-            </Form.Group>
-            <Header as='h3' dividing>Student Attendance</Header>
-            <Table compact celled unstackable>
-              <Table.Header>
-                <Table.Row>
-                  <Table.HeaderCell>Status</Table.HeaderCell>
-                  <Table.HeaderCell>Name</Table.HeaderCell>
-                </Table.Row>
-              </Table.Header>
-              <Table.Body>
-                {students.length === 0 &&
-                <Table.Row key={`student-notexist`}>
-                  <Table.Cell collapsing>
-                    <Checkbox checked={false} disabled />
-                  </Table.Cell>
-                  <Table.Cell>There are no students in this class.</Table.Cell>
-                </Table.Row>}
-                {students.length !== 0 && students.map((options, i) => (
-                  <Table.Row key={`student-${options.key}`}>
-                    <Table.Cell collapsing>
-                      <Checkbox name={options.key} onChange={handleCheckboxChangeForStudent} checked={options.status === 1} disabled={type !== 'Class'} />
-                    </Table.Cell>
-                    <Table.Cell>{options.text}</Table.Cell>
-                  </Table.Row>))}
-              </Table.Body>
-            </Table>
-
-            <Header as='h3' dividing>User Attendance</Header>
-
-            <Table compact celled unstackable>
-              <Table.Header>
-                <Table.Row>
-                  <Table.HeaderCell>Status</Table.HeaderCell>
-                  <Table.HeaderCell>Name</Table.HeaderCell>
-                </Table.Row>
-              </Table.Header>
-              <Table.Body>
-                {users.length === 0 &&
-                <Table.Row key={`user-notexist`}>
-                  <Table.Cell collapsing>
-                    <Checkbox checked={false} disabled />
-                  </Table.Cell>
-                  <Table.Cell>There are no users in this class.</Table.Cell>
-                </Table.Row>}
-                {users.length !== 0 && users.map((options, i) => (
-                  <Table.Row key={`user-${options.key}`}>
-                    <Table.Cell collapsing>
-                      <Checkbox name={options.key} onChange={handleCheckboxChangeForUser} checked={options.status === 1} disabled={type !== 'Class'} />
-                    </Table.Cell>
-                    <Table.Cell>{options.text}</Table.Cell>
-                  </Table.Row>))}
-              </Table.Body>
-            </Table>
-
+            <AddEditForm state={state} dispatch={dispatch} classData={classData} handleClass={handleClass} newAttendance edit />
             <Form.Button disabled={!classSelection} icon='plus' labelPosition='left' content='submit' />
           </Form>
         </Grid.Column>
